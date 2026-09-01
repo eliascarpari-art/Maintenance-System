ece33cc56530a6b7c8249022b7008df111cf94f8
ELIA DAVI SCARPARI <elia_scarpari@estudante.sc.senai.br>
2026-08-31 22:03:47 -0300

    aula 31/08

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,37 +4,37 @@ console.log("rodando o java script");
 
 const systemName = "Sistema de Controle de Manutenção";
 
-let activeEquipaments = 48;
+let activeEquipments = 48;
 
-let maintenanceEquipaments = 5;
+let maintenanceEquipments = 5;
 
 let preventiveMaintenance = 10;
 
 console.log("Nome do Sistema:" + systemName);
 
-console.info("Em Manutenção :" + maintenanceEquipaments);
+console.info("Em Manutenção :" + maintenanceEquipments);
 
 
-const equipaments = [
+const equipments = [
     {
         id:1,
         name: "Compressor",
         location : "Oficina",
-        status : "Ativo",
+        status : "active",
         patrimoy : "12-PP"
     },
     {
         id:2,
         name: "Torno",
         location : "Oficina",
-        status : "Ativo",
+        status : "active",
         patrimoy : "1-PP"
     },
     {
         id:3,
         name: "Gerador",
         location : "Casa de Maquinas",
-        status : "Manutenção",
+        status : "maintenance",
         patrimoy : "64-PP"
     }
 ];
@@ -42,42 +42,112 @@ const equipaments = [
 
 
 
-const activeTotal = document.getElementById("activesTotal").textContent = activeEquipaments;
+const activeTotal = document.getElementById("activesTotal").textContent = activeEquipments;
 
 const preventiveTotal = document.getElementById("preventivesTotal").textContent = preventiveMaintenance;
 
+const maintenance = document.getElementById("maintenanceEquipmentsTotal");
+
+const equipmentsTable = document.getElementById("equipmentsTable");
+
+const searchInput = document.getElementById("searchInput");
+
+const btnNewEquipament = document.getElementById("btnNewEquipment");
+
+const modalElement = document.getElementById("equipmentModal");
+
+const modal = new bootstrap.Modal(modalElement);
+
+const btnSave = document.getElementById("btnSaveEquipment");
 
 
-async function renderEquipaments() 
+
+function dashboardRefresh() 
 {
-    const tbody = document.getElementById("equipaments");
-  
+    const actives = equipments.filter(equipment => equipment.status === "active");
 
-    for (const element of equipaments) 
-    {
-        const tr = document.createElement("tr");
+    const inMaintenance = equipments.filter(equipment => equipment.status === "maintenance").length;
 
-        const tdName = document.createElement("td");
-        tdName.textContent = element.name;
+    activeTotal.textContent = actives;
+    maintenanceEquipmentsTotal.textContent = inMaintenance;
 
-        const tdLocation = document.createElement("td");
-        tdLocation.textContent = element.location;
+    console.log("Dashboard atualizado");
+}
 
-        const tdStatus = document.createElement("td");
-        tdStatus.textContent = element.status;
+dashboardRefresh();
 
-        tr.appendChild(tdName);
-        tr.appendChild(tdLocation);
-        tr.appendChild(tdStatus);
 
-        tbody.appendChild(tr);
-    }
+function equipmentsTableRender(list) 
+{
+    equipmentsTable.innerHTML = "";
     
+    list.forEach(equipment => {
+        const row = document.createElement("tr");
 
+        row.innerHTML = `
+            <td>${equipment.name}</td>
+            <td>${equipment.location}</td>
+            <td>${equipment.status}</td>
+            <td><button class = "btn-danger" onclick = "equipmentDelete(${equipment.id})">Excluir</button>
+            </td>
+        `;
+
+        equipmentsTable.appendChild(row);   
+    });
 }
+equipmentsTableRender(equipments);
 
-async function iniciar() {
-  await renderEquipaments();
+
+searchInput.addEventListener("input", function () {
+    const term = searchInput.value.toLowerCase();
+
+    const result = equipments.filter(equipment => equipment.name.toLocaleLowerCase().includes(term));
+
+    equipmentsTableRender(result);
+});
+
+
+btnNewEquipament.addEventListener("click" , function () {
+    modal.show();  
+})
+
+const equipmentName = document.getElementById("equipmentName");
+
+btnSave.addEventListener("click",function ()
+{
+    if (equipmentName.value.trim() === "") {
+        console.warn("Nome d equipamento não informado");
+        alert("Informe o nome do equipamento.");
+        return;
+    }  
+    
+    const newEquipment = {
+        id : equipments.length + 1,
+        name: equipmentName.value,
+        location : "Não Informador",
+        status : "active",
+        patrimoy: `${String(equipments.length + 1).padStart(3, "0")}-PP`
+    }
+
+    equipments.push(newEquipment);
+    equipmentsTableRender(equipments);
+    dashboardRefresh();
+
+    modal.hide();
+    equipmentName.value = "";
+})
+
+function equipmentDelete(id) 
+{
+    const index = equipments.findIndex(equipment => equipment.id === id);
+    if (index === 1) 
+    {
+        console.error("Equipamento não encontrador:", id);
+        return;    
+    }   
+    equipments.splice(index,1);
+    equipmentsTableRender(equipments);
+    dashboardRefresh();
+
+    console.log("Equipamento removido",id);
 }
-
-iniciar();
